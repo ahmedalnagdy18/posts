8d9207d400a128e798173d18059a5324a7c623c6
ahmedalnagdy18 <ahmedalnagdy188@gmail.com>
2024-04-21 17:35:37 +0200

    last commit

--- lib/features/posts/domain/entities/entity.dart
+++ lib/features/posts/domain/entities/entity.dart
@@ -4,9 +4,8 @@ class PostEntity extends Equatable {
   final int? skip;
   final int? limit;
 
-  PostEntity({this.skip, this.limit});
+  const PostEntity({this.skip, this.limit});
 
   @override
-  // TODO: implement props
   List<Object?> get props => [skip, limit];
 }
